44fe7408d4a2bf5e0a9294364ac9488a4255052e
Thanh Hung Le <elhungus@email.com>
2021-03-05 12:35:28 +0100

    Project list hover opacity

--- Components/ProjectList.js
+++ Components/ProjectList.js
@@ -40,12 +40,45 @@ export default function ProjectList() {
 		setTimeout(() => {
 			e.target.scrollIntoView({ behavior: 'smooth', block: 'start' })
 		}, 350);
+
+		e.target.parentElement.style.opacity === 1 
+			? e.target.parentElement.style.opacity = 0
+			: e.target.parentElement.style.opacity = 1
+	}
+
+	function returnCol(id, len) {
+		const colors = [
+			`linear-gradient(0deg, rgba(255,255,255,1) ${len}%, rgba(0,0,255,0.6) 100%)`,
+			`linear-gradient(0deg, rgba(255,255,255,1) ${len}%, rgba(0,255,139,0.6) 100%)`,
+			`linear-gradient(0deg, rgba(255,255,255,1) ${len}%, rgba(161,0,255,0.6) 100%)`,
+			`linear-gradient(0deg, rgba(255,255,255,1) ${len}%, rgba(255,0,0,0.6) 100%)`,
+		];
+
+		return colors[id % colors.length]
+	}
+
+	function onHover(e, isOpen) {
+		const el = e.target.parentElement;
+
+		if(!isOpen)
+			el.style.opacity === '0' ? el.style.opacity = 1 : el.style.opacity = 0
+
 	}
 
 	const p = projects.map(project => {
 			return (
-				<div key={project.id}>
-					<div className={styles.project_list_item_title} onClick={e => handleClick(e, project)}>
+				<div 
+					key={project.id} 
+					style={project.isOpen ? { backgroundImage: returnCol(project.id, 80) } : null}
+					className={styles.project_list_item}
+					onMouseEnter={e => onHover(e, project.isOpen)}
+					onMouseLeave={e => onHover(e, project.isOpen)}
+				>
+					<div 
+						className={styles.project_list_item_title}
+						onClick={e => handleClick(e, project)}
+						style={project.isOpen ? null : { backgroundImage: returnCol(project.id, 18) }}
+					>
 						{project?.title}
 					</div>
 					<Project isOpen={project.isOpen} />
