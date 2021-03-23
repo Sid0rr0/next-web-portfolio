f8053c22b92405b3fca759720c71037c4fd3cb3d
Thanh Hung Le <elhungus@email.com>
2021-03-23 19:23:59 +0100

    🍱 Using full resolution images

--- Components/Project.js
+++ Components/Project.js
@@ -8,8 +8,10 @@ import YouTube from 'react-youtube';
 export default function Project({ project }) {
 	//console.log(project)
 
+	{/* <StrapiImage image={image.formats?.large || image.formats.medium} imgClass="galleryImg" key={image.formats.large?.name || image.formats.medium.name} /> */ }
+
 	let carousel = project.Images.map(image => (
-		<StrapiImage image={image.formats?.large || image.formats.medium} imgClass="galleryImg" key={image.formats.large?.name || image.formats.medium.name} />
+		<StrapiImage image={image} imgClass="galleryImg" key={image.name} />
 	));
 
 	{/* <div className={styles.image} key={image.formats.large?.name || image.formats.medium.name}>
